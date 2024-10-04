806c12f6347ff048b309755dab4c7107ef22164c
Anna Oleksandrovych <150846802+anwalv@users.noreply.github.com>
2024-10-04 14:16:58 +0300

    added all figures

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
+
 using namespace std;
 
 const int BOARD_WIDTH = 80;
@@ -40,13 +42,13 @@ public:
 
 class Figure{
 public:
-    virtual void draw(Board& board, int x, int y, int size) = 0;
+    virtual void draw(Board& board, int x, int y, int height, int width) = 0;
 };
 
 class Triangle:Figure{
 public:
-    void draw(Board& board, int x, int y, int height) override {
-        if (height <= 0 || x < 1 || x >= BOARD_WIDTH - 1 || y < 1 || y + height >= BOARD_HEIGHT - 1) {
+    void draw(Board& board, int x, int y, int height, int width) override {
+        if (height <= 0 || width<0 || x < 1 || x >= BOARD_WIDTH - 1 || y < 1 || y + height >= BOARD_HEIGHT - 1) {
             cout<<"Invalid input!!!"<<endl;
             return;
         }
@@ -69,36 +71,102 @@ public:
 
 class Square:Figure{
 public:
-    void draw(Board& board, int x, int y, int size) override {
-        if (size <= 0 || x < 1 || x >= BOARD_WIDTH - 1 || y < 1 || y + size >= BOARD_HEIGHT - 1) {
+    void draw(Board& board, int x, int y, int height, int width) override {
+        if (height <= 0 || width<0 || x < 1 || x >= BOARD_WIDTH - 1 || y < 1 || y + height >= BOARD_HEIGHT - 1) {
             cout << "Invalid input!!!" << endl;
             return;
         }
-        for (int i = 0; i < size; ++i) {
+        for (int i = 0; i < height; ++i) {
             board.setCell(x, y + i, '*');
-            board.setCell(x + size - 1, y + i, '*');
+            board.setCell(x + height - 1, y + i, '*');
         }
-        for (int j = 0; j < size; ++j) {
+        for (int j = 0; j < height; ++j) {
             board.setCell(x + j, y, '*' );
-            board.setCell(x + j, y + size - 1, '*');
+            board.setCell(x + j, y + height - 1, '*');
         }
     }
 };
 
-class Rectangle:Figure{};
+class Rectangle:Figure{
+public:
+    void draw(Board& board, int x, int y,int height, int width) override{
+        if (width <= 0 || height <= 0 || x < 1 || x + width >= BOARD_WIDTH - 1 ||
+            y < 1 || y + height >= BOARD_HEIGHT - 1) {
+            cout << "Invalid input!!!" << endl;
+            return;
+        }
+        for (int i =0; i<width; i++){
+            board.setCell(x+i, y, '*');
+            board.setCell(x+i, y + height-1, '*');
+        }
+        for(int j = 0; j<height; j++){
+            board.setCell(x, y+j,'*');
+            board.setCell(x+width-1, y+j, '*');
+        }
+    }
+};
 
-class Line:Figure{};
+class Line:Figure{
+public:
+    void draw(Board& board,int x, int y, int height, int width) override{
+        if (height==0&width==0||height < 0|| width<0 || x < 1 || x >= BOARD_WIDTH - 1 || y < 1 || y + height >= BOARD_HEIGHT - 1){
+            cout<<"Invalid input!!!"<<endl;
+        }
+        if (width == 0){
+            for (int i = 0; i < height; i++) {
+                board.setCell(x + i, y, '*');
+            }
+        }
+        else if(height==0){
+            for(int i = 0; i<width; i++){
+                board.setCell(x,y+i, '*');
+            }
+        }
+    }
+};
 
-class Manager{};
+class Circle:Figure{
+public:
+
+    void draw(Board& board, int x, int y, int radius, int width) override {
+        if (radius <= 0 || width<0 || x - radius < 1 || x + radius >= BOARD_WIDTH - 1 ||
+            y - radius < 1 || y + radius >= BOARD_HEIGHT - 1) {
+            cout << "Invalid input!!!" << endl;
+            return;
+        }
+
+        for (int i = 0; i <= radius; i++) {
+            for (int j = 0; j <= radius; j++) {
+                if (round(sqrt(i * i + j * j)) == radius) {
+                    board.setCell(x + j, y + i, '*');
+                    board.setCell(x - j, y + i, '*');
+                    board.setCell(x + j, y - i, '*');
+                    board.setCell(x - j, y - i, '*');
+                }
+            }
+        }
+    }
+
+};
+
+class Manager{
+   //коли лінія то питати вертикальна чи горизонтальна
+};
 
 class Writer{};
 int main() {
     Board board(BOARD_WIDTH, BOARD_HEIGHT);
     Triangle triangle;
-    triangle.draw(board, 40, 5, 5);
+    triangle.draw(board, 10, 8, 3, 0);
+    Circle circle;
+    circle.draw(board, 6, 4, 3, 0);
     Square square;
-    square.draw(board, 40, 8, 5);
-
+    square.draw(board, 40, 8, 5, 0);
+    Rectangle rectangle;
+    rectangle.draw(board, 4,15,3,8);
+    Line line;
+    line.draw(board, 15, 18, 0, 4);
+    line.draw(board, 17, 16, 4, 0);
     board.draw();
     return 0;
 }
